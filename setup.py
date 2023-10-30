e7ba0bc66e865e3356481152cb32ed8d2230b32f
JPGodTier <145616322+JPGodTier@users.noreply.github.com>
2023-10-30 11:37:22 +0100

    modified setup reqs

--- setup.py
+++ setup.py
@@ -2,16 +2,16 @@ from setuptools import setup, find_packages
 
 setup(
     name="TaskMe",
-    version="0.0.1",
+    version="0.1",
     packages=find_packages(),
     install_requires=[
-        "colorama == 0.4.6",
-        "coverage == 7.3.2",
-        "iniconfig == 2.0.0",
-        "packaging == 23.2",
-        "pluggy == 1.3.0",
-        "pytest == 7.4.3",
-        "pytest - cov == 4.1.0",
-        "pytest - mock == 3.12.0"
+        "colorama==0.4.6",
+        "coverage==7.3.2",
+        "iniconfig==2.0.0",
+        "packaging==23.2",
+        "pluggy==1.3.0",
+        "pytest==7.4.3",
+        "pytest-cov == 4.1.0",
+        "pytest-mock == 3.12.0"
     ]
 )
